23b0fee2b04dcd32bd397b1a5c876c9497a20503
Javid Majidzade <mecidzade.cavid@mail.ru>
2023-06-16 19:37:17 +0400

    changed Tk to Toplevel

--- final.py
+++ final.py
@@ -1,39 +1,36 @@
-
 from tkinter import *
+
+def open_plate():
+    root2 = Toplevel()
+    root2.title("Plate")
+    root2.geometry("1200x770")
+    plate11 = PhotoImage(file="Images/plate.png")
+    plate1 = Label(root2, bg='peachpuff1', image=plate11)
+    plate1.place(x=0, y=0)
+    root2.mainloop()
+
+
 root = Tk()
 root.title("My Restaurant")
 root.geometry("1573x899")
-
-
-
-def open_plate():
-    root2 = Tk()
-    root2.title("Plate")
-    root2.geometry("1200x770")
-    plate11 = PhotoImage(file="Picsart_23-06-15_23-51-38-798.png")
-    plate1 = Label(root2, bg='peachpuff1', image=plate11)
-    plate1.place(x=150, y=400)
-
-
-
-backphotowin1 = PhotoImage(file="stock-photo-texture-beige-color-paper-background.png")
-table2_1 = PhotoImage(file="Rec 2.png")
-table2_2 = PhotoImage(file="Rec 2.png")
-table2_3 = PhotoImage(file="Rec 2.png")
-table2_4 = PhotoImage(file="Rec 2.png")
-table4_1 = PhotoImage(file="Round 4.png")
-table4_2 = PhotoImage(file="Round 4.png")
-table6_1 = PhotoImage(file="Rec 6.png")
-table6_2 = PhotoImage(file="Rec 6.png")
-kenar11 = PhotoImage(file="635e9e0327dcbc383d0eb2b6-artiflr-6-ft-artificial-peony-garland - Köçür.png")
-kenar22 = PhotoImage(file="635e9e0327dcbc383d0eb2b6-artiflr-6-ft-artificial-peony-garland.png")
-kenar33 = PhotoImage(file="635e9e0327dcbc383d0eb2b6-artiflr-6-ft-artificial-peony-garland.png")
-kenar44 = PhotoImage(file="635e9e0327dcbc383d0eb2b6-artiflr-6-ft-artificial-peony-garland - Köçür.png")
-plant11 = PhotoImage(file="169-1691852_organic-quality-takes-time.png")
-plant22 = PhotoImage(file="169-1691852_organic-quality-takes-time.png")
-kassa1 = PhotoImage(file="Picsart_23-06-15_22-47-04-631.png")
-warehouse1 = PhotoImage(file="Picsart_23-06-15_23-01-47-610.png")
-shop1 = PhotoImage(file="Picsart_23-06-15_23-17-52-508.png")
+backphotowin1 = PhotoImage(file="Images/stock-photo-texture-beige-color-paper-background.png")
+table2_1 = PhotoImage(file="Images/Rec 2.png")
+table2_2 = PhotoImage(file="Images/Rec 2.png")
+table2_3 = PhotoImage(file="Images/Rec 2.png")
+table2_4 = PhotoImage(file="Images/Rec 2.png")
+table4_1 = PhotoImage(file="Images/Round 4.png")
+table4_2 = PhotoImage(file="Images/Round 4.png")
+table6_1 = PhotoImage(file="Images/Rec 6.png")
+table6_2 = PhotoImage(file="Images/Rec 6.png")
+kenar11 = PhotoImage(file="Images/635e9e0327dcbc383d0eb2b6-artiflr-6-ft-artificial-peony-garland - Köçür.png")
+kenar22 = PhotoImage(file="Images/635e9e0327dcbc383d0eb2b6-artiflr-6-ft-artificial-peony-garland.png")
+kenar33 = PhotoImage(file="Images/635e9e0327dcbc383d0eb2b6-artiflr-6-ft-artificial-peony-garland.png")
+kenar44 = PhotoImage(file="Images/635e9e0327dcbc383d0eb2b6-artiflr-6-ft-artificial-peony-garland - Köçür.png")
+plant11 = PhotoImage(file="Images/169-1691852_organic-quality-takes-time.png")
+plant22 = PhotoImage(file="Images/169-1691852_organic-quality-takes-time.png")
+kassa1 = PhotoImage(file="Images/Picsart_23-06-15_22-47-04-631.png")
+warehouse1 = PhotoImage(file="Images/Picsart_23-06-15_23-01-47-610.png")
+shop1 = PhotoImage(file="Images/Picsart_23-06-15_23-17-52-508.png")
 
 
 back1 = Label(root,image= backphotowin1)
@@ -72,13 +69,5 @@ plant1 = Label(root,bg= 'peachpuff1', image= plant11)
 plant1.place(x=1000, y=150)
 plant1 = Label(root,bg= 'peachpuff1', image= plant22)
 plant1.place(x=1000, y=500)
-
-
-
-
-
-
-
-
 
 root.mainloop()
--- kldnfkfjdv.py
+++ kldnfkfjdv.py
@@ -6,7 +6,7 @@ root.geometry("1000x1500")
 
 
 image1 = PhotoImage(file ="dxml55nt.png")
-image2 = PhotoImage(file ="Rec 2.png")
+image2 = PhotoImage(file ="Images/Rec 2.png")
 
 job5 = Label(master=root,bd=5, image=image1, font=1)
 job5.place(x=0, y=0)
